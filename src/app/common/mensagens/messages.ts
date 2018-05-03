dd4309c5ded4c59247b564fc917e99276d8963dd
Italo <italobrazil7@gmail.com>
2018-05-02 22:30:40 -0300

    Exclusão e Detalhes do Curso

--- src/app/common/mensagens/messages.ts
+++ src/app/common/mensagens/messages.ts
@@ -1,8 +1,16 @@
 export class SysMessages {
-    public static messages = [{
-        id: 1,
-        description: "Inclusão realizada com sucesso."
-    }];
+    public static messages = [
+        {
+            id: 1,
+            description: "Inclusão realizada com sucesso."
+        }, {
+            id: 2,
+            description: "Alteração realizada com sucesso."
+        }, {
+            id: 3,
+            description: "Exclusão realizada com sucesso."
+        }
+    ];
 
     public static get(id: Number): string {
         return this.messages.find(message => message.id === id).description;
