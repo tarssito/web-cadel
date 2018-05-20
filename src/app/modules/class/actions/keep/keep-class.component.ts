5027134c072cc138549e5f9eeb633bcbfb27ee56
Italo <italobrazil7@gmail.com>
2018-05-20 20:54:57 -0300

    Manter turma

--- src/app/modules/class/actions/keep/keep-class.component.ts
+++ src/app/modules/class/actions/keep/keep-class.component.ts
@@ -1,19 +1,66 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { SysMessages } from './../../../../common/mensagens/messages';
+
+import { AlertService } from './../../../../directives/alert/shared/alert.service';
+import { LoadingService } from './../../../../directives/loading/shared/loading.service';
+
+import { Course } from './../../../course/shared/course.model';
+import { CourseService } from './../../../course/shared/course.service';
+
+import { Subject } from './../../../subject/shared/subject.model';
+import { SubjectService } from './../../../subject/shared/subject.service';
+
+import { Class } from './../../shared/class.model';
+import { ClassService } from './../../shared/class.service';
+
+import { Student } from './../../../student/shared/student.model';
+import { StudentService } from './../../../student/shared/student.service';
 
 @Component({
   selector: 'app-keep-class',
   templateUrl: './keep-class.component.html'
 })
 export class KeepClassComponent {
+  currentYear: number;
   labelBtn: String;
   title: String;
   mySource: any;
   selectedStudent: any;
+  class: Class;
+  courseList: Course[];
+  subjectList: Subject[];
+  periodList: any[];
+  ageList: any[];
 
-  constructor( private _sanitizer: DomSanitizer) {
+  constructor(
+    private _sanitizer: DomSanitizer,
+    private location: Location,
+    private router: Router,
+    private activateRoute: ActivatedRoute,
+    private subjectService: SubjectService,
+    private alertService: AlertService,
+    private loadingService: LoadingService,
+    private courseService: CourseService,
+    private classService: ClassService,
+    private studentService: StudentService
+  ) {
+    //init
+    this.currentYear = new Date().getFullYear();
+    this.class = new Class();
+    this.courseList = [];
+    this.subjectList = [];
+    this.periodList = [];
+    this.ageList = [];
     this.title = "Incluir Turma";
     this.labelBtn = "Incluir";
+    // this.successCode = 1;
+    this.loadCourses();
+    this.loadPeriod();
+    this.loadAge();
+    // this.detail();
 
     this.mySource = [{
       id: 1,
@@ -26,8 +73,50 @@ export class KeepClassComponent {
     }];
   }
 
+  private loadCourses(): void {
+    this.loadingService.loading(true);
+    this.courseService.list(new Course())
+      .subscribe(data => {
+        this.courseList = <Course[]>data;
+        this.loadingService.loading(false);
+      }, error => {
+        this.alertService.error(error);
+      });
+  }
+
+  private loadSubject(): void {
+    this.loadingService.loading(true);
+    this.subjectService.list(new Subject())
+      .subscribe(data => {
+        this.subjectList = <Subject[]>data;
+        this.loadingService.loading(false);
+      }, error => {
+        this.alertService.error(error);
+      });
+  }
+
+  onChangeCourse() {
+    this.class.disciplina = new Subject();
+    this.subjectList = [];
+    if (this.class.curso.id) {
+      this.loadSubject();
+    }
+  }
+
+  private loadPeriod(): void {
+    this.periodList.push({ id: 1, label: 1 });
+    this.periodList.push({ id: 2, label: 2 });
+  }
+
+  private loadAge(): void {
+    for (let i = 0; i < 10; i++) {
+      this.ageList.push({ id: this.currentYear + i, label: this.currentYear + i });
+    }
+  }
+
+
   public findStudents() {
-    return { data: { results: []} };
+    return { data: { results: [] } };
   }
 
   public renderStudent(data: any): SafeHtml {
--- src/app/modules/class/shared/class.model.ts
+++ src/app/modules/class/shared/class.model.ts
@@ -1,4 +1,5 @@
 import { Course } from './../../course/shared/course.model';
+import { Subject } from './../../subject/shared/subject.model';
 
 export class Class {
     id: number;
@@ -6,8 +7,10 @@ export class Class {
     semestre: number;
     ano: number;
     curso: Course;
+    disciplina: Subject;
 
     constructor() {
         this.curso = new Course();
+        this.disciplina = new Subject();
     }
 } 
